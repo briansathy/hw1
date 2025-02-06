d1e6013ab8a50e8106392874d529cffb74cbf301
Brian Sathy <bsathy@usc.edu>
2025-02-06 06:41:40 +0000

    finished :)

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -1,13 +1,47 @@
+using namespace std;
 #include <string>
 #include <vector>
 #include <iostream>
 #include <sstream>
 
 #include "ulliststr.h"
-
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+ULListStr dat;
+// Checks to see if popping empty list works
+dat.pop_back();
+dat.pop_front();
+dat.push_back("asd");
+dat.push_front("bips");
+dat.push_back("book");
+// Checks the legitamacy of 
+cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
+dat.pop_back();
+dat.pop_back();
+cout << dat.back() << endl;
+cout << dat.back() << "and is of the size: " << dat.size() << endl;
+cout << dat.size() << endl;  
 
+dat.push_front("the_front");
+cout << dat.front();
+
+dat.push_back("the_back");
+cout << dat.back();
+
+dat.pop_back();
+
+cout << dat.front() << endl;
+
+std::string test = dat.front();
+cout << test << endl;
+
+dat.push_back(test);
+dat.push_front(test);
+
+cout << dat.front() << dat.back() << endl;
 }
+
+
+
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,154 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{
+  // If list is empty, initialize as head and tail
+  if (tail_ == NULL)
+  {
+  
+    tail_ = new Item();
+    head_ = tail_;
+    tail_->first = 0;
+    tail_->last = 0;
+  }
+  // If tail is full, allocate a new item, and update pointers
+  if (tail_ -> last == ARRSIZE)
+  {
+    Item* newItem = new Item();
+    newItem -> prev = tail_;
+    // tail_->first = 0;
+    // tail_->last = 0;
+    newItem->first = 0;
+    newItem->last = 0;
+    tail_ -> next = newItem;
+    tail_ = newItem;
+  }
+  // Insert new value
+  tail_ -> val[tail_ -> last] = val;
+  tail_ -> last++;
+  size_++;
+}
+void ULListStr::pop_back()
+{
+  // Checks if list is empty, if not, it updates the back
+  if (empty())
+  {
+    return;
+  }
+  tail_->last--;
+  size_--;
+  // If the tail node is empty, remove it from list
+  if(tail_->first == tail_->last)
+  {
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if (tail_ != NULL)
+    {
+      tail_->next = NULL;
+    }
+    else
+    {
+      head_ = NULL; // reset head
+    }
+    delete temp;
+  }
+}
+void ULListStr::push_front(const std::string& val)
+{
+  // Initialize new item
+  if (head_ == NULL)
+  {
+    head_ = new Item();
+    tail_ = head_;
+    head_->first = ARRSIZE;
+    head_->last = ARRSIZE;
+  }
+  // ALlocate a new item if head is full
+  if (head_ -> first == 0)
+  {
+    Item* newItem = new Item();
+    newItem -> next = head_;
+    head_ -> prev = newItem;
+    head_ = newItem;
+    head_->first = ARRSIZE;
+    head_->last = ARRSIZE;
+  }
+  // Update the list
+  head_ -> first--;
+  head_ -> val[head_ -> first] = val;
+  size_++;
+}
+void ULListStr::pop_front()
+{
+  // Checks if list is empty
+  if (empty())
+  {
+    return;
+  }
+  head_->first++;
+  size_--;
+  // Remove empty node if necessary
+  if (head_->first == head_->last)
+  {
+    Item* temp = head_;
+    head_ = head_->next;
+    if (head_ != NULL)
+    {
+      head_->prev = NULL;
+    }
+    else
+    {
+      tail_ = NULL; // Reset tail
+    }
+    delete temp;
+  }
+}
+std::string const & ULListStr::back() const
+{
+  // Checks for range error, and then returns proper indexed value
+  if (empty())
+  {
+    throw std::out_of_range("Empty list");
+  }
+  return tail_->val[(tail_ -> last) -1];
+}
+std::string const & ULListStr::front() const
+{
+  // Checks for range error, and then returns proper indexed value
+  if (empty())
+  {
+    throw std::out_of_range("Empty list");
+  }
+  return head_->val[head_->first];
+}
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  // Checks if out of bounds
+  if (loc >= size_)
+  {
+    return NULL;
+  }
+  Item* temp = head_;
+  size_t index = loc;
+  // Loops through to find proper element, then indexes accordingly
+  while (temp != NULL)
+  {
+    size_t block = temp -> last - temp -> first;
+    if (index < block)
+    {
+      return &(temp -> val[temp->first + index]);
+    }
+    index -= block;
+    temp = temp -> next;
+  }
+  return NULL;
+}
+
+
+
+
+
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
